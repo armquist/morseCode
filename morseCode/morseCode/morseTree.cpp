3fcd3ba33510955cc248cb6874fed411366b875a
ayrahca007 <55465855+ayrahca007@users.noreply.github.com>
2019-12-05 17:34:44 -0600

    Update morseTree.cpp
    I added the decode function

--- morseCode/morseCode/morseTree.cpp
+++ morseCode/morseCode/morseTree.cpp
@@ -15,8 +15,49 @@ void morseTree::encode(string word) {
 
 }
 
-void morseTree::decode(string code) {
+void morseTree::decode(string code) { //decodes morse code into alphanumeric letters: O(n)
+	cout << "Decoded String: " << endl; //clean formatting
+	string tempCode; //temporary string to push to decodeLetter rfunction
+	tempCode.resize(code.size()); //resize strings to be equal to prevent crashes
+	int c = 0; //extra counter to copy user input morse code to tempCode
+	for (int i = 0; i < code.length(); i++) {  //read till the end of the string
+		tempCode[c] += code[i]; //take each char from input string and put it into tempcode
+		if (tempCode[c] == ' ') { //push tempcode to decodeLetter and clear out tempcode once string is decoded
+			decodeLetter(tempCode); //actually traverse tree to get letter
+			c = 0; //reset counter for next slew of morsecode chars
+			tempCode = ""; //clear temcode
+			tempCode.resize(code.size()); //resize strings to be equal to prevent crashes
+		}
+		if (i == code.length() - 1) { //special case for very last character as not to force user to add space at the end of code input
+			tempCode += " "; //add a space to make decodeLetter's job easier
+			decodeLetter(tempCode); //actually traverse tree to get letter
+			c = 0; //reset counter for next slew of morsecode chars
+			tempCode = ""; //clear temcode
+		}
+		c++; //increase counter to keep copying from input string
+	}
+	cout << endl; //end line for cleanliness
+}
 
+void morseTree::decodeLetter(string code) { //Traverse tree to reach letter decoded morse string: O(n)
+	node* curentNode = root; //set root every time to ensure traversal consistancy
+	string decodedMessage = ""; //translated letter
+	for (int i = 0; i < code.length(); i++) { //will keep traversing for the length of the current morse code bit
+		if (code[i] == '_') { //traverse right for every _
+			if (curentNode->right != NULL) { //prevent freak accidents 
+				curentNode = curentNode->right; //traverse right
+			}
+		}
+		else if (code[i] == '.') { //traverse left for every .
+			if (curentNode->left != NULL) { //prevent freak accidents 
+				curentNode = curentNode->left; //traverse left
+			}
+		}
+		else if (code[i] == ' ') { //space means stop traversing
+			decodedMessage += curentNode->data; //set decodedMessage to current node
+		}
+	}
+	cout << decodedMessage; //print out our decoded letter
 }
 
 void morseTree::buildTree(ifstream& istream) { //actually builds the Huffman Tree for use with the other funtions O(n)
